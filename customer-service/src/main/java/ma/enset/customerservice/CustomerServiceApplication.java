8d5342a5d891ac865f074b5172cd82efe3d08778
aliloumehdi <mehdialilou99@gmail.com>
2023-12-04 23:10:34 +0100

    Config Service Implementation

--- customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -1,15 +1,18 @@
 package ma.enset.customerservice;
 
+import ma.enset.customerservice.config.GlobalConfig;
 import ma.enset.customerservice.entities.Customer;
 import ma.enset.customerservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
