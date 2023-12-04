44894c0e8dd059c37e30fcd163048e2c9bbb6609
aliloumehdi <mehdialilou99@gmail.com>
2023-12-04 20:57:03 +0100

    POC : Skeleton + Services init

--- customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,41 @@
 package ma.enset.customerservice;
 
+import ma.enset.customerservice.entities.Customer;
+import ma.enset.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+        return args -> customerRepository.saveAll(
+                Arrays.asList(
+                        Customer.builder()
+                                .firstName("Mehdi")
+                                .lastName("ALILOU")
+                                .email("mehdialilou99@gmail.com")
+                                .build(),
+                        Customer.builder()
+                                .firstName("Amine")
+                                .lastName("Doum")
+                                .email("amine@gmail.com")
+                                .build(),
+                        Customer.builder()
+                                .firstName("Jean")
+                                .lastName("Park")
+                                .email("jean@gmail.com")
+                                .build()
+                ));
     }
 
 }
